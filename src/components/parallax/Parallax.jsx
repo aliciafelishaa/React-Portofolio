26d1a22acadc3904b315fd8d3de7a117c78757f5
aliciafelishaa <aliciafelisha67@gmail.com>
2025-02-18 04:02:05 +0700

    additional

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -25,7 +25,7 @@ function Parallax({ type }) {
               : "linear-gradient(180deg, #0c0c1d, #505064)",
         }}
       >
-        <motion.h1 style={{y:yText}}>{type === "services" ? "What I Do?" : "What I Did?"}</motion.h1>
+        <motion.h1 style={{y:yText}}>{type === "services" ? "What I Do?" : "My Portofolio"}</motion.h1>
         <motion.div className="mountains"></motion.div>
         <motion.div className="planets"  style={{y:yBg, backgroundImage: `url(${type==="services"?"./planets.png":"./sun.png"})`}}></motion.div>
         <motion.div className="stars" style={{x:yBg}}></motion.div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -26,27 +26,63 @@ function Services() {
           </div>
         </motion.div>
         <motion.div className="listContainer">
-          <motion.div className="box" whileHover={{backgroundColor:"rgb(29, 14, 44)", border:"2px solid rgb(29, 14, 44)"}}>
-            <motion.h2 >TechnoScape Event</motion.h2>
+          <motion.div
+            className="box"
+            whileHover={{
+              backgroundColor: "rgb(117, 170, 193)",
+              border: "2px solid rgb(117, 170, 193)",
+            }}
+          >
+            <motion.h2>BNCC TECHNOSCAPE 2024</motion.h2>
             {/* <button>My contributions</button> */}
           </motion.div>
-          <motion.div className="box" whileHover={{backgroundColor:"rgb(29, 14, 44)", border:"2px solid rgb(29, 14, 44)"}}>
+          <motion.div
+            className="box"
+            whileHover={{
+              backgroundColor: "rgb(117, 170, 193)",
+              border: "2px solid rgb(117, 170, 193)",
+            }}
+          >
             <h2>BNCC Techno Talk 2024</h2>
             {/* <button>My contributions</button> */}
           </motion.div>
-          <motion.div className="box" whileHover={{backgroundColor:"rgb(29, 14, 44)", border:"2px solid rgb(29, 14, 44)"}}>
+          <motion.div
+            className="box"
+            whileHover={{
+              backgroundColor: "rgb(117, 170, 193)",
+              border: "2px solid rgb(117, 170, 193)",
+            }}
+          >
             <h2>BNCC Corporate Social Responsibility</h2>
             {/* <button>My contributions</button> */}
           </motion.div>
-          <motion.div className="box" whileHover={{backgroundColor:"rgb(29, 14, 44)", border:"2px solid rgb(29, 14, 44)"}}>
+          <motion.div
+            className="box"
+            whileHover={{
+              backgroundColor: "rgb(117, 170, 193)",
+              border: "2px solid rgb(117, 170, 193)",
+            }}
+          >
             <h2>HIMTI Techno Transcend Welcoming Party</h2>
             {/* <button>My contributions</button> */}
           </motion.div>
-          <motion.div className="box" whileHover={{backgroundColor:"rgb(29, 14, 44)", border:"2px solid rgb(29, 14, 44)"}}>
+          <motion.div
+            className="box"
+            whileHover={{
+              backgroundColor: "rgb(117, 170, 193)",
+              border: "2px solid rgb(117, 170, 193)",
+            }}
+          >
             <h2>HIMTI SEMINAR</h2>
             {/* <button>My contributions</button> */}
           </motion.div>
-          <motion.div className="box" whileHover={{backgroundColor:"rgb(29, 14, 44)", border:"2px solid rgb(29, 14, 44)"}}>
+          <motion.div
+            className="box"
+            whileHover={{
+              backgroundColor: "rgb(117, 170, 193)",
+              border: "2px solid rgb(117, 170, 193)",
+            }}
+          >
             <h2>HIMTI Company Visit</h2>
             {/* <button>My contributions</button> */}
           </motion.div>
