776ca510a96101d774b2f87de8d39ff785b7642c
aliciafelishaa <aliciafelisha67@gmail.com>
2025-02-18 23:58:06 +0700

    upcommit

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,39 +1,86 @@
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import "./services.scss";
+import { useRef } from "react";
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 1,
+    opacity: 1,
+
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 function Services() {
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
     <>
-      <motion.div className="services">
-        <motion.div className="textContainer">
+      <motion.div
+        className="services"
+        initial="initial"
+        variants={variants}
+        // whileInView="animate"
+        ref={ref}
+        // animate={isInView && "animate"}
+        animate={isInView ? "animate" : "initial"}
+      >
+        <motion.div className="textContainer" variants={variants}>
           <p>
             I was an software engineer enthusiast <br />
             who growth by learning
           </p>
           <hr />
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div
+          className="titleContainer"
+          initial="initial"
+          animate="animate"
+          variants={variants}
+        >
           <div className="title">
             <h1>
-              <b>Here</b> are
+              <motion.b whileHover={{ color: "rebeccapurple" }}>Here</motion.b>{" "}
+              are
             </h1>
           </div>
-          <div className="title">
+          <motion.div className="title">
             <h1>
-              My <b>Organization</b> Experience.
+              My{" "}
+              <motion.b whileHover={{ color: "rebeccapurple" }}>
+                Organization
+              </motion.b>{" "}
+              Experience.
             </h1>
-            <button>Let's Take A Look!</button>
-          </div>
+            {/* <button>Let's See!</button> */}
+          </motion.div>
         </motion.div>
-        <motion.div className="listContainer">
+        <motion.div
+          className="listContainer"
+          initial="initial"
+          animate="animate"
+          variants={variants}
+        >
           <motion.div
+            initial="initial"
+            animate="animate"
+            variants={variants}
             className="box"
             whileHover={{
-              backgroundColor: "rgb(117, 170, 193)",
+              backgroundColor: "rgb(156, 225, 255)",
               border: "2px solid rgb(117, 170, 193)",
             }}
           >
-            <motion.h2>BNCC TECHNOSCAPE 2024</motion.h2>
+            <h2>BNCC TECHNOSCAPE 2024</h2>
             {/* <button>My contributions</button> */}
           </motion.div>
           <motion.div
@@ -59,7 +106,7 @@ function Services() {
           <motion.div
             className="box"
             whileHover={{
-              backgroundColor: "rgb(117, 170, 193)",
+              backgroundColor: "rgb(156, 225, 255)",
               border: "2px solid rgb(117, 170, 193)",
             }}
           >
@@ -79,7 +126,7 @@ function Services() {
           <motion.div
             className="box"
             whileHover={{
-              backgroundColor: "rgb(117, 170, 193)",
+              backgroundColor: "rgb(156, 225, 255)",
               border: "2px solid rgb(117, 170, 193)",
             }}
           >
